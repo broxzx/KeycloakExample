2b32f24a15ac27b6ae95366da0a2268fcfbac927
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-04-14 18:19:38 +0200

    Update Oauth2 configuration from Resource Server to Client

--- src/main/java/com/messageservice/keycloakexample/config/SecurityConfiguration.java
+++ src/main/java/com/messageservice/keycloakexample/config/SecurityConfiguration.java
@@ -5,23 +5,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
 public class SecurityConfiguration {
 
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                        .anyRequest().authenticated())
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+                        .anyRequest().hasRole("MANAGER"))
+                .oauth2Login(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        OidcUserService oidcUserService = new OidcUserService();
+
+        return userRequest -> {
+            OidcUser oidcUser = oidcUserService.loadUser(userRequest);
+            List<String> group = Optional.ofNullable(oidcUser.getClaimAsStringList("groups"))
+                    .orElseGet(List::of);
+            List<SimpleGrantedAuthority> authorities = group
+                    .stream()
+                    .filter(role -> role.startsWith("ROLE_"))
+                    .map(SimpleGrantedAuthority::new)
+                    .toList();
+
+            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+        };
     }
 
 }
--- src/main/java/com/messageservice/keycloakexample/controller/HomeController.java
+++ src/main/java/com/messageservice/keycloakexample/controller/HomeController.java
@@ -1,16 +1,21 @@
 package com.messageservice.keycloakexample.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("api")
+@Log4j2
 public class HomeController {
 
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Principal principal) {
+        log.info(principal);
         return "Hello World!";
     }
 }
